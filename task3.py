9e4634b6ad4ae23b86e3fdd11d2bff04a7b9ddcb
mackanh123 <98534262+mackanh123@users.noreply.github.com>
2023-01-24 14:08:07 +0100

    latest version task 3 A1

--- task3.py
+++ task3.py
@@ -1,12 +1,19 @@
 import numpy as np
 from scipy.linalg import eigh
+import matplotlib.pyplot as plt
 
 # Define the grid
-N = 101
+N = 1000
 r_min = 0
-r_max = 5
-r = np.linspace(r_min, r_max, N)
-dr = r[1] - r[0]
+r_max = 10
+r = np.zeros((N))
+dr = (r_max-r_min)/N
+for i in range(N):
+    r[i] = r_min + i*dr
+#r = np.linspace(r_min, r_max, N)
+#dr = r[1] - r[0]
+
+
 
 # Create a Hamiltonian matrix
 H = np.zeros((N,N)) 
@@ -27,18 +34,49 @@ def V_c(r):
 # Fill the Hamiltonian matrix based on the finite differences
 for i in range(1,N-1):
     H[i, i-1] = -1 / (2 * dr**2)
-    H[i, i] = 1 / dr**2 + V_H(r[i]) + V_x(r[i]) + V_c(r[i])
+    H[i, i] = 1 / dr**2 -1/r[i]#- 2/r[i] + V_H(r[i]) + V_x(r[i]) + V_c(r[i])
     H[i, i+1] = -1 / (2 * dr**2)
 
 # Implement the boundary conditions
-H[0,0] = 1
-H[-1,-1] = 1
+H[0,0] = 0
+H[-1,-1] = 0
+H[1,0] = 0
+
+
+#print(H)
 
 #print(H)
 
 # Solve the eigenvalue problem
 E, C = eigh(H)
-print(E)
+print(E[0]*27.21132)
+print(E[0])
+
+# Solving for the wavefunction phi
+u = C[:,0]
+#print(u)
+phi = u / (np.sqrt(4*np.pi)*r)
+phi = np.delete(phi, 0)
+r = np.delete(r, 0)
+#print(phi)
+
+integral = np.trapz(phi)
+#print(integral)
+phi = phi / integral
+
+
+# The normalized hydrogen ground state wavefunction
+def psi(r):
+    return 2*np.exp(-r)/(2*np.sqrt(np.pi))    
+
+psi_hydrogen = psi(r)
+
+plt.plot(r, psi_hydrogen, 'r--')
+
+plt.plot(r, phi)
+
+#print(psi_hydrogen)
+plt.show()
 
 
 
