77869e2416e607f352074748717c8090cdc9f896
mackanh123 <98534262+mackanh123@users.noreply.github.com>
2023-01-26 12:48:28 +0100

    Delete tools.h

--- tools.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-void velocity_verlet(int n_timesteps, int n_particles, double v[][3], double position[][3],
-double position_evolution[][n_particles][3], double dt, double m, double force[][3], 
-double cell_length);
-
-void write_to_file(char *fname, double *times,
-            double *pot_energy, double *kin_energy, 
-            double *tot_energy, int n_points);
