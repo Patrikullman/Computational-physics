6e596e76413412e501f4890028657fc22fe30bf0
Patrikullman <107335671+Patrikullman@users.noreply.github.com>
2023-01-31 09:55:58 +0100

    Add files via upload

--- jobscript.sh
+++ jobscript.sh
@@ -1,28 +1,3 @@
-#use this to log in to Vera : ssh patriku@vera2.c3se.chalmers.se 
-
-#!/urs/bin/env bash
-#SBATCH -A SNIC2021-7-120
-#SBATCH -t 12:00:00
-#SBATCH --job-name= Na6 cluster program    #This is just a name so we can recognize it on the queue list 
-#SBATCH -N 1 # Use 1 node
-#SBATCH -n 1 # Use only 1 core on that node
-
-
-model purge #this clears the environment from previous modules. 
-module load python3
-module load intel/2019a GPAW ASE
-
-#run the code:
-gpaw-python ga.py
-#./ga.py ? 
-#srun ./ga.py ? 
-
-# skicka in detta i terminalen för att submitta jobbet : sbatch jobscript.sh
-# squeue JOB ID to see information 
-
-
-########################################################################################################################
-
 #!/usr/bin/env bash
 #SBATCH -A C3SE2023-2-2 # Project
 #SBATCH -J GPAW # Name of the job
