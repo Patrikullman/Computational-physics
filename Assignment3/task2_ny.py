6cd29623ca1a158173831ed2a100e01d08a0c01b
Patrikullman <107335671+Patrikullman@users.noreply.github.com>
2023-02-10 12:29:22 +0100

    Add files via upload

--- Assignment3/task2_ny.py
+++ Assignment3/task2_ny.py
@@ -1,74 +1,57 @@
-from ase.io import read
-from ase import Atoms
-from ase.visualize import view
-from gpaw import GPAW
-from ase.md.npt import NPT
-from ase.io.trajectory import TrajectoryReader
 import numpy as np
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
+from ase.io.trajectory import Trajectory
+from ase import Atoms
+
+atoms = Trajectory('NaCluster24.traj') #someDynamics NaCluster24
+
+distances = [] #np.array([])
+for i in range(1000,14000):   #i think we need to remove the first time frames since we need to pick frames after equilibration.
+    distances = np.append(distances,atoms[i].get_distances(72,atoms[i].get_atomic_numbers()==8,mic=True))
+
+histogram = np.histogram(distances,bins=1000)
+
+V = atoms[0].get_volume()  #cell volume, this is an ase function. 
+N = 24 #oxygen molecules
+rho = N/V #number density, see formula lecture notes (third lecture by Julia Wiktor, on RDF formula)
+
+counts = histogram[0]            #histogram[0] gives us an array with binnes values, notice histogram[1] gives the bin_edges, and you can also do hist[][] for indexing since its 2 dimensional.
+counts = np.array(counts)     
+counts = np.append(counts,1)  # i just use this to get the right dimensions when dividing in the rdf but we can probably solve it in a better way than this. 
+
+dr = histogram[1][1]-histogram[1][0] #thickness of a shell, the indexing is used to extract bin_edges. Notice for example histogram[1][1] gives second bin_edge and [1][0] gives first bin_edge. 
+print("thickness of a shell dr",dr)
+r = histogram[1] - dr #this gives me bin_edge and subtracts the thickness from the shell so effectively we get the radius of each shell. imagine we have for example 4th shell. we subtract thicness to get radius of third shell and so on.
 
 
-traj = TrajectoryReader('NaCluster24.traj') 
-all_distances = []
-for i in range(13999):
-    #print(traj[i][72]) # First index is timestep, second index is atom
-    atoms_current_time_frame = traj[i]#[72]
-    #print(atoms_current_time_frame[i])
-    current_pos = atoms_current_time_frame.get_positions()
-    Na_pos = current_pos[72]
-    #print(Na_pos)
+#r, dr and rho gives us all info we need to calculate volume of each shell. We now calculate the rdf by taking counts divided by the increasing shell volumes.
 
-    O_pos_array = []
-    for j in range(24):
-        O_pos = current_pos[j]
-        O_pos_array.append(O_pos)
+rdf = counts / ((4/3*np.pi*r**2*rho*dr)*len(atoms) ) #formula from third lecture by Julia Wiktor, on RDF)
 
-    O_pos_array = np.array(O_pos_array)
-    #print(O_pos_array.shape)
-    L = 8.95
-    #dist_array = []
-    for j in range(24):
-        diff_x = (Na_pos[0] - O_pos_array[j][0]) - L * np.round((Na_pos[0] - O_pos_array[j][0])/L) #calculates all distances, and also takes minimum image convention into consideration.
-        diff_y = (Na_pos[1] - O_pos_array[j][1]) - L * np.round((Na_pos[1] - O_pos_array[j][1])/L)
-        diff_z = (Na_pos[2] - O_pos_array[j][2]) - L * np.round((Na_pos[2] - O_pos_array[j][2])/L)
-        dist = np.sqrt(diff_x**2 + diff_y**2 + diff_z**2)
-        #dist_array.append(dist)
-        all_distances.append(dist)
-    #dist_array = np.array(dist_array)
-all_distances = np.array(all_distances)
-    
-print(all_distances.shape)
+integral = 0
+for i, r_value in enumerate(r):
+    if r_value <= 3.14:
+        integral += rdf[i] * dr 
+print("Integral value up to r = 3.14:", integral)
 
-bins = 1000
-N = 24
-
-counts, bin_edges = np.histogram(all_distances, bins)
-bin_centers = (bin_edges[:-1] + bin_edges[1:])/2
-
-rdf = counts/(4/3 * np.pi * (bin_centers**3))
-plt.plot(bin_centers, rdf)
+plt.plot(r,rdf)
 plt.show()
 
 
-x = []
-y = []
-i = 0
-while bin_centers[i] < 3.14:
-    x.append(bin_centers[i])
-    y.append(rdf[i])
-    i += 1
-    
-
-def integrate(x, y):
-    area = np.trapz(y=y, x=x)
-    return area
-
-area = integrate(x,y)
-print(area)
 
 
 
-# some comments :   we only need to calculcate RDF for the relevant species.  that means we only need RDF for sodium-oxygen. We dont need to consider hydrogen. 
+#----------------------below is not important----------------------------#
 
 
+#print("RDF",rdf)
+#print("Volume",V)
+#print("number of particles",N)
+#print("Histogram[0] and also counts",counts)
 
+#print("r is the radius of a shell: ",r)
+#hist[0] gives an array with binned values
+#hist[1] gives an array with bin_edges.
+#with [0][1] we get binned values, numbers in first bin. 
+#print(np.shape(r))
+#print(np.shape(counts))
